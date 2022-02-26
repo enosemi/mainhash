5aeb131d0d2f1b73cd415b4289faea7f1d465564
enosemi <42423968+enosemi@users.noreply.github.com>
2022-02-26 21:20:51 +0100

    Update settings.py

--- e_food/settings.py
+++ e_food/settings.py
@@ -11,8 +11,8 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
-django_heroku.settings(locals())
+
+
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
